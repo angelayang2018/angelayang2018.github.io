59c5ee01cd817975894b1ad47d26fb40dc6b286c
angelayang2018 <legaanangy@gmail.com>
2024-01-16 02:18:06 -0800

    Fix issue with routes by installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Project} from "./pages/Project";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  require("react-dom");
+  window.React2 = require("react");
+  console.log(window.React1 === window.React2);
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
