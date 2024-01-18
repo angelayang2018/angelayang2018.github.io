328671d98f2f4808a5b8880d622f8e978cf58853
angelayang2018 <legaanangy@gmail.com>
2024-01-18 11:32:17 -0800

    Navbar fixed position

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
   });
 
   return (
-    <div>
+    <div className = "home">
       <div className = "intro">
         <div>
           <p>Frontend Developer | Software Engineer</p>
