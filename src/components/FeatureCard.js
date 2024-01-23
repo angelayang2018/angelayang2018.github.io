1857d4d859cd0310f774088b4d4475eb3c853a52
angelayang2018 <legaanangy@gmail.com>
2024-01-23 15:59:10 -0800

    Organize feature styling

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export default function FeatureCard({feature}) {
-    const { title, description, source, alt } = feature;
+export default function FeatureCard({ feature }) {
+  const { title, description, source, alt } = feature;
 
   return (
     <div className="featureCard">
       <h4>{title}</h4>
       <p>{description}</p>
-      <div className="featureImg">
-        <img src={source} alt = {alt} />
-      </div>
+      <img src={source} alt={alt} />
     </div>
   );
 }
--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -4,39 +4,35 @@ import { Link } from "react-router-dom";
 export default function ProjectDetails({ projectInfo }) {
   const { title, overview, team, tech, timeline, github } = projectInfo;
   return (
-    <div>
+    <div className="projectDetails">
       <Link to="/project">
         <p>← Back to Projects</p>
       </Link>
       <h2>{title}</h2>
-      <div className="projectOverview">
-        <p>{overview}</p>
-      </div>
-      <div className="projectTech">
-        <p>
-          Team: Angela Yang
-          {team.map((member, index) => (
-            <span>
-              ,{" "}
-              <a
-                className="credit"
-                target="_blank"
-                rel="noreferrer"
-                href={member.linkedin}
-              >
-                {member.name}
-              </a>
-            </span>
-          ))}
-        </p>
-        <p>Technologies Used: {tech}</p>
-        <p>Timeline: {timeline}</p>
-        <p>
-          <a href={github} target="_blank" rel="noreferrer" className="buttonS">
-            Github
-          </a>
-        </p>
-      </div>
+      <p>{overview}</p>
+      <p>
+        Team: Angela Yang
+        {team.map((member, index) => (
+          <span>
+            ,{" "}
+            <a
+              className="credit"
+              target="_blank"
+              rel="noreferrer"
+              href={member.linkedin}
+            >
+              {member.name}
+            </a>
+          </span>
+        ))}
+      </p>
+      <p>Technologies Used: {tech}</p>
+      <p>Timeline: {timeline}</p>
+      <p>
+        <a href={github} target="_blank" rel="noreferrer" className="buttonS">
+          Github
+        </a>
+      </p>
     </div>
   );
 }
